2c4486f31aa582067c09e90070ddc8583e27d4f3
Hueslei <234298003+huesleim@users.noreply.github.com>
2026-05-17 19:06:21 -0300

    Correção de gráfico de AQI das cidades; adicionados gráficos de investimento público e espaços verdes ao longo dos anos.

--- app/dashboard.py
+++ app/dashboard.py
@@ -19,11 +19,31 @@ with col2:
 
 df_filtrado = df[(df["year"] == ano_selecionado) & (df["city"] == cidade_selecionada)]
 
+
+# Declaração de variáveis para os gráficos
+cidades_brasileiras = df[df["country"] == "Brazil"]["city"].unique()
+
+ano_df = df[df["year"] == ano_selecionado]
+cidade_df = df[df["city"] == cidade_selecionada]
+
+aqi_por_cidade = ano_df.groupby("city")["aqi"].mean().reset_index()
+aqi_por_cidade = aqi_por_cidade[(aqi_por_cidade["city"].isin(cidades_brasileiras)) |
+(aqi_por_cidade["city"] == cidade_selecionada)]
+
+aqi_por_ano = cidade_df.groupby("year")["aqi"].mean().reset_index()
+
+espaços_verdes_por_ano = cidade_df.groupby("year")["green_space_ratio_%"].mean().reset_index()
+investimento_por_ano = cidade_df.groupby("year")["env_budget_million_usd"].mean().reset_index()
+
+
 # Criar abas/páginas
 aba = st.tabs(["📊 Visão Geral", "🌱 Poluentes", "📈 Evolução"])
 
 # Aba 1 - Visão Geral
 with aba[0]:
+    st.subheader("⚙️Conteúdo da base de dados")
+    st.write(df_filtrado.dtypes)
+
     st.subheader("📊 Dados filtrados")
     st.write(df_filtrado.head())
 
@@ -37,7 +57,14 @@ with aba[1]:
     with col1:
         st.subheader(f"Média do AQI por cidade - Ano {ano_selecionado}")
         fig, ax = plt.subplots()
-        df[df["year"] == ano_selecionado].groupby("city")["aqi"].mean().plot(kind="bar", ax=ax, color="skyblue")
+
+
+        ax.bar(
+            aqi_por_cidade["city"],
+            aqi_por_cidade["aqi"],
+            color="skyblue"
+        )
+
         ax.set_ylabel("AQI médio")
         st.pyplot(fig)
 
@@ -55,8 +82,83 @@ with aba[1]:
 
 # Aba 3 - Evolução
 with aba[2]:
+
+
+    # Evolução do AQI ao longo dos anos
     st.subheader(f"📈 Evolução do AQI ao longo dos anos - {cidade_selecionada}")
     fig, ax = plt.subplots()
-    df[df["city"] == cidade_selecionada].groupby("year")["aqi"].mean().plot(kind="line", marker="o", ax=ax, color="red")
+    ax.plot(
+        aqi_por_ano["year"], 
+        aqi_por_ano["aqi"], 
+        label="AQI", 
+        color="red", 
+        marker="o"
+    )
     ax.set_ylabel("AQI médio")
+    ax.set_xlabel("Ano")
+    st.pyplot(fig)
+
+    # Relação entre espaços verdes e AQI ao longo dos anos
+    st.subheader( f"📉 Relação entre espaços verdes e AQI ao longo dos anos - {cidade_selecionada}")
+    fig, ax1 = plt.subplots(figsize=(10, 5))
+
+    # Espaços verdes
+    ax1.plot(
+    espaços_verdes_por_ano["year"],
+    espaços_verdes_por_ano["green_space_ratio_%"],
+    color="green",
+    marker="o",
+    label="Espaços Verdes (%)"
+    )
+
+    ax1.set_xlabel("Ano")
+    ax1.set_ylabel("Espaços Verdes (%)", color="green")
+    ax1.tick_params(axis="y", labelcolor="green")
+
+    # AQI
+    ax2 = ax1.twinx()
+
+    ax2.plot(
+        aqi_por_ano["year"],
+        aqi_por_ano["aqi"],
+        color="red",
+        marker="o",
+        label="AQI"
+    )
+
+    ax2.set_ylabel("AQI", color="red")
+    ax2.tick_params(axis="y", labelcolor="red")
+
+    st.pyplot(fig)
+
+    # Relação entre investimento governamental em sustentabilidade e AQI ao longo dos anos
+    st.subheader( f"📉 Relação entre investimento público e AQI ao longo dos anos - {cidade_selecionada}")
+    fig, ax1 = plt.subplots()
+
+    # Investimento governamental
+    ax1.plot(
+    investimento_por_ano["year"],
+    investimento_por_ano["env_budget_million_usd"],
+    color="green",
+    label="Investimento público em políticas ambientais (USD)"
+    )
+
+    ax1.set_xlabel("Ano")
+    ax1.set_ylabel("Investimento público em políticas ambientais (USD)", color="green")
+    ax1.tick_params(axis="y", labelcolor="green")
+
+    # AQI
+    ax2 = ax1.twinx()
+
+    ax2.plot(
+        aqi_por_ano["year"],
+        aqi_por_ano["aqi"],
+        color="red",
+        marker="o",
+        label="AQI"
+    )
+
+    ax2.set_ylabel("AQI", color="red")
+    ax2.tick_params(axis="y", labelcolor="red")
+
     st.pyplot(fig)
